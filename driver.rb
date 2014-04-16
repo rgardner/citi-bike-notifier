5421f16773e179af91fd154a56a3ca1a7742544d
Robert Gardner <rhg259@nyu.edu>
2014-04-16 12:54:48 -0400

    Fix trip message and add success message

--- driver.rb
+++ driver.rb
@@ -29,6 +29,8 @@ unless user.login(username, password) == LOGIN_SUCCESS
 end
 
 log "Successfully logged in to Citi Bike website\n"
+send_message('Welcome to Citi Bike Notifier! You are now setup to receive a ' \
+             'message after returning a Citi Bike.')
 
 loop do
   log "#{DateTime.now}: "
@@ -41,7 +43,7 @@ loop do
   # check if ride completed in last SLEEP_DURATION secs
   if (DateTime.now.to_time.to_i - trip.end_time.to_time.to_i) < SLEEP_DURATION
     log 'Recent trip found! '
-    notify_me
+    notify_me(trip)
   else
     log 'No recent trip found. '
   end
