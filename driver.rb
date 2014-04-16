366a145dbe4cf7d67138b9b630f32c43bde165ad
Robert Gardner <rhg259@nyu.edu>
2014-04-16 09:46:12 -0400

    Fix logging

--- driver.rb
+++ driver.rb
@@ -10,12 +10,15 @@ require_relative 'bike_trip'
 require_relative 'citi_scraper'
 require_relative 'notify'
 
-$stdout.reopen('out.log', 'w')
-$stderr.reopen('err.log', 'w')
-
 LOGIN_SUCCESS = 'Welcome To Citi Bike!'
 CITIBIKE_CONFIG = YAML.load_file(File.join(__dir__, 'config.yml'))
 SLEEP_DURATION = 300 # in seconds
+
+def log(message)
+  File.open('out.log', 'a') do |f|
+    f.printf(message)
+  end
+end
 
 user = CitiScraper.new
 username = CITIBIKE_CONFIG['citibike_username']
@@ -25,10 +28,10 @@ unless user.login(username, password) == LOGIN_SUCCESS
   fail 'Incorrect username or password'
 end
 
-puts 'Successfully logged in to Citi Bike website'
+log "Successfully logged in to Citi Bike website\n"
 
 loop do
-  print "#{DateTime.now}: "
+  log "#{DateTime.now}: "
   trip = user.trips.first
   unless trip
     user.login(username, password)
@@ -37,12 +40,12 @@ loop do
 
   # check if ride completed in last SLEEP_DURATION secs
   if (DateTime.now.to_time.to_i - trip.end_time.to_time.to_i) < SLEEP_DURATION
-    print 'Recent trip found! '
+    log 'Recent trip found! '
     notify_me
   else
-    print 'No recent trip found. '
+    log 'No recent trip found. '
   end
 
-  puts "Sleeping for #{SLEEP_DURATION} seconds."
+  log "Sleeping for #{SLEEP_DURATION} seconds.\n"
   sleep(SLEEP_DURATION)
 end
