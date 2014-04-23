94002c8d5ceea7f222761f5aac6a5cf71f7174ac
Robert Gardner <rhg259@nyu.edu>
2014-04-22 21:47:12 -0400

    Use time which assumes local timezone

--- bike_trip.rb
+++ bike_trip.rb
@@ -8,7 +8,6 @@
 class BikeTrip
   DATE_FORMAT = '%m/%d/%y %l:%M:%S %p'       # citibike format as of 4/15/2013
   DURATION_REGEX = /(\d{1,2})m (\d{1,2})s/   # citibike format as of 4/15/2013
-  TIMEZONE_OFFSET = 4 * 60 * 60              # EST is 4 hrs behind UTC
   SECS_PER_MIN = 60
 
   attr_accessor :id, :start_location, :start_time, :end_location, :end_time,
@@ -16,18 +15,16 @@ class BikeTrip
 
   # Converts raw datetime string to DateTime object
   def start_time=(datetime)
-    @start_time = DateTime.strptime(datetime, DATE_FORMAT)
-    @start_time = (@start_time.to_time - TIMEZONE_OFFSET).to_datetime
+    @start_time = Time.strptime(datetime, DATE_FORMAT)
   end
 
   def end_time=(datetime)
-    @end_time = DateTime.strptime(datetime, DATE_FORMAT)
-    @end_time = (@start_time.to_time - TIMEZONE_OFFSET).to_datetime
+    @end_time = Time.strptime(datetime, DATE_FORMAT)
   end
 
   # Duration of trip in seconds
   def duration=(duration)
-    if duration.is_a? Integer
+    if duration.is_a?(Integer)
       @duration = duration
     else
       minutes, seconds = DURATION_REGEX.match(duration).captures
