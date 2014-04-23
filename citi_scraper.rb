0a1d9451fbdf03db0231913face4ee7bbabd3390
Robert Gardner <rhg259@nyu.edu>
2014-04-22 22:44:19 -0400

    Refactor citi scraper

--- citi_scraper.rb
+++ citi_scraper.rb
@@ -9,41 +9,61 @@ require_relative 'bike_trip'
 
 # Scrape the Citibike website
 class CitiScraper
-  def initialize
+  LoginError = Class.new(StandardError)
+
+  LOGIN_URL = 'https://citibikenyc.com/login'
+  TRIPS_URL = 'https://citibikenyc.com/member/trips'
+  LOGIN_SUCCESS = 'Welcome To Citi Bike!'
+  TRIPS_SUCCESS = 'Trips | Citi Bike'
+
+  attr_accessor :username, :password
+
+  # initialize variables and login
+  def initialize(username, password)
     @agent = Mechanize.new
+    @username = username
+    @password = password
+    login
   end
 
-  def login(username, password)
-    @agent.get('https://citibikenyc.com/login')
+  def login(username = @username, password = @password)
+    @agent.get(LOGIN_URL)
     @agent.page.forms[0]['subscriberUsername'] = username
     @agent.page.forms[0]['subscriberPassword'] = password
     @agent.page.forms[0].submit
-    @agent.page.title
+    unless @agent.page.title == LOGIN_SUCCESS
+      fail LoginError, 'Invalid username or password.'
+    end
+    @password = password
+    @username = username
   end
 
-  # Returns this month's trips. Returns nil if timeout.
+  # Returns this month's trips.
   def trips
-    return nil unless @agent.click('Trips').title['Trips']
+    @agent.get(TRIPS_URL)
+    unless @agent.page.title == TRIPS_SUCCESS
+      login
+      @agent.get(TRIPS_URL)
+    end
     rows = Nokogiri::HTML(@agent.page.body).xpath('//table/tbody/tr')
-    # reject rows with invalid date or durations < 2 minutes
+
+    # reject rows with durations < 2 minutes
     rows = rows.reject do |row|
-      row.at_xpath('td[5]/text()').to_s.strip.empty? ||
-              row.at_xpath('td[6]/text()').to_s.match(/(\d{1,2})m/)[1].to_i < 2
+      row.at_xpath('td[6]/text()').to_s.match(/(\d{1,2})m/)[1].to_i < 2
     end
-    trips = rows.map do |row|
-      trip = BikeTrip.new
-      [
-        [:id, 'td[1]/text()'],
-        [:start_location, 'td[2]/text()'],
-        [:start_time, 'td[3]/text()'],
-        [:end_location, 'td[4]/text()'],
-        [:end_time, 'td[5]/text()'],
-        [:duration, 'td[6]/text()']
-      ].each do |name, xpath|
-        trip.send("#{name}=", row.at_xpath(xpath).to_s.strip)
-      end
-      trip
+    rows.map { |row| row_to_trip(row) }
+  end
+
+  private
+
+  # Convert HTML row into bike trip object
+  def row_to_trip(row)
+    trip = BikeTrip.new
+    trip_attributes = [:id, :start_location, :start_time, :end_location,
+                       :end_time, :duration]
+    trip_attributes.each_with_index do |name, i|
+      trip.send("#{name}=", row.at_xpath("td[#{i + 1}]/text()").to_s.strip)
     end
-    trips
+    trip
   end
 end
--- driver.rb
+++ driver.rb
@@ -10,7 +10,6 @@ require_relative 'bike_trip'
 require_relative 'citi_scraper'
 require_relative 'notify'
 
-LOGIN_SUCCESS = 'Welcome To Citi Bike!'
 CITIBIKE_CONFIG = YAML.load_file(File.join(__dir__, 'config.yml'))
 SLEEP_DURATION = 600 # in seconds
 
@@ -20,13 +19,9 @@ def log(message)
   end
 end
 
-user = CitiScraper.new
 username = CITIBIKE_CONFIG['citibike_username']
 password = CITIBIKE_CONFIG['citibike_password']
-
-unless user.login(username, password) == LOGIN_SUCCESS
-  fail 'Incorrect username or password'
-end
+user = CitiScraper.new(username, password)
 
 log "Successfully logged in to Citi Bike website\n"
 Notify.send_message('Welcome to Citi Bike Notifier! You are now setup to ' \
@@ -34,12 +29,7 @@ Notify.send_message('Welcome to Citi Bike Notifier! You are now setup to ' \
 
 loop do
   log "#{DateTime.now}: "
-  trips = user.trips
-  unless trips
-    user.login(username, password)
-    trips = user.trips
-  end
-  trip = trips.first
+  trip = user.trips.first
 
   # check if ride completed in last SLEEP_DURATION secs
   if (DateTime.now.to_time.to_i - trip.end_time.to_time.to_i) < SLEEP_DURATION
